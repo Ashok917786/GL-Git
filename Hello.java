861f446f9f416b09a2cca2d321efd570690f2f5a
Ashok917786 <67753129+Ashok917786@users.noreply.github.com>
2022-05-16 02:42:41 +0530

    sample

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int num=10;
+        int num1=15;
+        int num2=20;
+        int total= num+ num1+num2;
+        System.out.println(total);
+        int last= 1000-total;
+        System.out.println(last);
+    }
+}
+
+
